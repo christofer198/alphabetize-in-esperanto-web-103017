b012aea430008137c80d056eb245f3cb9227ace3
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-10-24 15:16:30 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
   # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".each_char.with_index.to_a.to_h
+    arr.sort_by do |phrase|
+        phrase.chars.collect do |char|
+            esperanto_alphabet[char]
+        end
+    end
 end
